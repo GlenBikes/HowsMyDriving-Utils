bf908fd9bbe82908a44bbe64e3a3c11a014374ed
Glitch (howsmydriving-utils) <none>
2019-11-23 00:14:12 +0000

    Initial checkin.

---
+++ src/citations.js
@@ -0,0 +1,10 @@
+const MINIMUM_CITATION_ID = 0,
+  noPlateFoundCitationNumber = -1,
+  noCitationsFoundCitationNumber = -2;
+
+
+module.exports = {
+  CitationIDNoPlateFound: noPlateFoundCitationNumber,
+  CitationIDNoCitationsFound: noCitationsFoundCitationNumber,
+  MINIMUM_CITATION_ID: MINIMUM_CITATION_ID
+}
