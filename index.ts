2da1f99163cb96627fcd052f657b5313cb57158b
Glitch (howsmydriving-utils) <none>
2019-12-29 08:43:07 +0000

    Retry of duplicate tweets seems to be generally working.

--- index.ts
+++ index.ts
@@ -1,13 +1,45 @@
-import * as packpath from 'packpath';
+const packpath = require('packpath');
+const appRootDir = require('app-root-dir').get();
+const appRootPath = require('app-root-path');
+const appRoot = require('app-root');
+
 import * as path from 'path';
+
+let packpath_parent = packpath.parent() ? packpath.parent() : packpath.self();
+let packpath_self = packpath.self();
+
+console.log(
+  `howsmydriving-utils:\n - packpath_self: ${packpath_self}\n - packpath_parent: ${packpath.parent()}\n - app-root-dir: ${appRootDir}\n - app-root-path: ${appRootPath}\n - app-root (current): see below\n - app-root (root): see below\n - __dirname: ${__dirname}\n - .: ${path.resolve(
+    '.'
+  )}`
+);
+
+appRoot({
+  directory: '.',
+  success: function(roots) {
+    console.log(`howsmydriving-utils:\n - app-root (current): ${roots}`);
+  }
+});
+
+appRoot({
+  directory: '/',
+  success: function(roots) {
+    console.log(`howsmydriving-utils:\n - app-root (root): ${roots}`);
+  }
+});
+
+export const log4js_config_path = path.resolve(
+  appRootDir + '/dist/config/log4js.json'
+);
+
+console.log(
+  `howsmydriving-utils: log4js_config_path:\n - ${log4js_config_path}`
+);
 
 import './src/util/extend/string-ext';
 import { DumpObject } from 'howsmydriving-utils';
 
 import { log } from './src/logging';
-
-let packpath_parent = packpath.parent() ? packpath.parent() : packpath.self();
-let packpath_self = packpath.self();
 
 const package_json_path = path.join(packpath_self, '/package.json');
 
--- src/logging.ts
+++ src/logging.ts
@@ -1,11 +1,43 @@
-import { sleep } from 'howsmydriving-utils';
-
 const packpath = require('packpath');
+const appRootDir = require('app-root-dir').get();
+const appRootPath = require('app-root-path');
+const appRoot = require('app-root');
 
-const __MODULE_NAME__ = 'HowsMyDriving-Utils';
+import * as path from 'path';
 
 let packpath_parent = packpath.parent() ? packpath.parent() : packpath.self();
 let packpath_self = packpath.self();
+let package_config_path = path.resolve(packpath_self + '/package.json');
+
+console.log(
+  `howsmydriving-twitter:\n - packpath_self: ${packpath_self}\n - packpath_parent: ${packpath.parent()}\n - app-root-dir: ${appRootDir}\n - app-root-path: ${appRootPath}\n - app-root (current): see below\n - app-root (root): see below\n - __dirname: ${__dirname}\n - .: ${path.resolve(
+    '.'
+  )}`
+);
+
+appRoot({
+  directory: '.',
+  success: function(roots) {
+    console.log(`howsmydriving-twitter:\n - app-root (current): ${roots}`);
+  }
+});
+
+appRoot({
+  directory: '/',
+  success: function(roots) {
+    console.log(`howsmydriving-twitter:\n - app-root (root): ${roots}`);
+  }
+});
+
+export const log4js_config_path = path.resolve(
+  appRootDir + '/dist/config/log4js.json'
+);
+
+console.log(
+  `howsmydriving-twitter: log4js_config_path:\n - ${log4js_config_path}`
+);
+
+import { sleep } from 'howsmydriving-utils';
 
 /**
  *  Standard logging for HowsMyDriving modules.
@@ -16,8 +48,11 @@ let packpath_self = packpath.self();
  **/
 var log4js = require('log4js'),
   chokidar = require('chokidar'),
-  path = require('path'),
-  config_path = path.join(packpath_parent + '/dist/config/log4js.json');
+  config_path = path.join(packpath_parent + '/dist/config/log4js.json'),
+  pjson = require(package_config_path);
+
+const __MODULE_NAME__ = pjson.name;
+const __MODULE_VERSION__ = pjson.version;
 
 // Load the config.
 log4js.configure(config_path);
@@ -27,6 +62,9 @@ log4js.configure(config_path);
 export var log = log4js.getLogger('result');
 
 log.addContext('module', __MODULE_NAME__);
+log.info(
+  `howsmydriving-utils: package_config_path: ${package_config_path}, __MODULE_NAME__: ${__MODULE_NAME__} __MODULE_VERSION__: ${__MODULE_VERSION__}.`
+);
 
 import { CitationIds } from './citationIds';
 
